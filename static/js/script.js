a7690ff6addb0f19788e566688f24f3eb2452721
ethandonlon <94150522+ethandonlon@users.noreply.github.com>
2022-08-23 15:41:47 -0400

    added clipping for image

--- static/js/script.js
+++ static/js/script.js
@@ -1,16 +1,12 @@
-var nav_height = document.getElementsByClassName("nav-parent")[0].offsetHeight;
-var header = document.getElementsByTagName("Header")[0];
-console.log(header.offsetHeight);
-console.log(window.screen.height-nav_height);
-if(header.offsetHeight <= window.screen.height-nav_height) {
-  header.style.height = "calc(100vh - var(--header-height))";
-}
 window.addEventListener("load", function() {
+  adjust_header_height();
+  round_overflow_container();
   document.querySelector(".page-loader").classList.add("fade-out");
   setTimeout(() => {
     document.querySelector(".page-loader").style.display = "none";
   }, 600);
 });
+
 /**
  * Animation for down-arrow button
  */
@@ -21,3 +17,27 @@ var animation = bodymovin.loadAnimation({
   autoplay: true,
   path: "/static/animation/down_arrow.json",
 });
+
+function adjust_header_height() {
+  var nav_height = document.getElementsByClassName("nav-parent")[0].offsetHeight;
+  var header = document.getElementsByTagName("Header")[0];
+  console.log(header.offsetHeight);
+  console.log(window.screen.height-nav_height);
+  
+  if(header.offsetHeight <= window.screen.height-nav_height) {
+    header.style.height = "calc(100vh - var(--header-height))";
+  }
+}
+
+function round_overflow_container() {
+  var containers = document.getElementsByClassName("overflow-container");
+  console.log(containers);
+  for(let i = 0; i < containers.length; i++) {
+    if (containers[i].parentElement.style.order > 0) {
+      containers[i].classList.add("round-xxlarge-right");
+    } else {
+      containers[i].classList.add("round-xxlarge-left");
+    }
+  }
+
+}
