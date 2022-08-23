5adf46bc1955fe048dd3e2c22a049a9e4d94d820
ethandonlon <94150522+ethandonlon@users.noreply.github.com>
2022-08-23 15:03:49 -0400

    adding screen responsive for macbook

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,10 @@
+var nav_height = document.getElementsByClassName("nav-parent")[0].offsetHeight;
+var header = document.getElementsByTagName("Header")[0];
+console.log(header.offsetHeight);
+console.log(window.screen.height-nav_height);
+if(header.offsetHeight <= window.screen.height-nav_height) {
+  header.style.height = "calc(100vh - var(--header-height))";
+}
 window.addEventListener("load", function() {
   document.querySelector(".page-loader").classList.add("fade-out");
   setTimeout(() => {
